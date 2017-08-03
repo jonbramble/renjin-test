77f7a5177b76954d83d1883a433b39744ceab648
Jonathan Bramble <jbramble82@gmail.com>
2017-08-03 10:30:48 -0700

    initial commit

---
+++ src/main/java/TryRenjin.java
@@ -0,0 +1,17 @@
+import javax.script.*;
+import org.renjin.script.*;
+
+// ... add additional imports here ...
+
+public class TryRenjin {
+  public static void main(String[] args) throws Exception {
+    // create a script engine manager:
+    RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+    // create a Renjin engine:
+    ScriptEngine engine = factory.getScriptEngine();
+
+    engine.eval("df <- data.frame(x=1:10, y=(1:10)+rnorm(n=10))");
+    engine.eval("print(df)");
+    engine.eval("print(lm(y ~ x, df))");
+  }
+}
